fe1edccf7dc12d9855738482d5a8529e5791a56f
Ivan <vanya71310@gmail.com>
2019-12-22 16:26:59 +0300

    Начинаем стилизацию второй страницы

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
   var drop_menu = document.querySelector('.drop-menu');
+  drop_menu.classList.remove('drop-menu--nojs');
   var tgl_btn = document.querySelector('.page-header__logo-toogle');
   var cls_btn = document.querySelector('.drop-menu__toogle-btn');
   var hidden_rate = document.querySelector('.hidden-rates');
@@ -7,6 +8,8 @@ document.addEventListener('DOMContentLoaded',function(){
   var hide_rate_btn = document.querySelector('.hidden-rates');
   var logo_header = document.querySelector('.page-header__logo-image');
   var logo_header_drop = document.querySelector('.page-header__logo-image-drop');
+  var letter_list = document.querySelectorAll('.country-list__letter');
+  var country_list = document.querySelector('.country-list');
 
   tgl_btn.addEventListener('click',function(){
     drop_menu.classList.add('drop-menu--active');
@@ -16,12 +19,27 @@ document.addEventListener('DOMContentLoaded',function(){
     drop_menu.classList.remove('drop-menu--active');
   });
 
-  show_rate_btn.addEventListener('click', function(){
-    hidden_rate.classList.add('hidden-rates--show');
+ // show_rate_btn.addEventListener('click', function(){
+ //   hidden_rate.classList.add('hidden-rates--show');
+ // });
+
+ // hide_rate_btn.addEventListener('click', function(){
+ //   hidden_rate.classList.remove('hidden-rates--show');
+ // });
+
+  country_list.addEventListener('click', function(event){
+    for (var i = 0; i < letter_list.length; i++) {
+      letter_list[i].classList.remove('country-list__letter--active');
+      event.target.classList.add('country-list__letter--active');
+    }
   });
 
-  hide_rate_btn.addEventListener('click', function(){
-    hidden_rate.classList.remove('hidden-rates--show');
-  });
+  //letter_list.addEventListener('click', function(){
+  //  console.log("1");
+  //  for (var i = 0; i <= letter_list.length; i++) {
+  //    letter_list[i].classList.remove('country-list__letter--active');
+  //  }
+  //})
+
 
 });
