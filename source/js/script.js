ed90e4ceaa57cbd6e0b7bc805c8eb4784997ff89
Ivan <vanya71310@gmail.com>
2019-12-28 12:25:33 +0300

    Верстка 2-й страницы

--- source/js/script.js
+++ source/js/script.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded',function(){
   var logo_header_drop = document.querySelector('.page-header__logo-image-drop');
   var letter_list = document.querySelectorAll('.country-list__letter');
   var country_list = document.querySelector('.country-list');
+  var filter = document.querySelector('.filter');
+  var open_filter = document.querySelector('.filter__open-filter');
+  var close_filter = document.querySelector('.filter__close-filter');
+  var fellow_filter = document.querySelector('.fellow-filter');
 
   tgl_btn.addEventListener('click',function(){
     drop_menu.classList.add('drop-menu--active');
@@ -19,27 +23,47 @@ document.addEventListener('DOMContentLoaded',function(){
     drop_menu.classList.remove('drop-menu--active');
   });
 
- // show_rate_btn.addEventListener('click', function(){
- //   hidden_rate.classList.add('hidden-rates--show');
- // });
+  if (show_rate_btn){
+    show_rate_btn.addEventListener('click', function(){
+      hidden_rate.classList.add('hidden-rates--show');
+    });
+  }
 
- // hide_rate_btn.addEventListener('click', function(){
- //   hidden_rate.classList.remove('hidden-rates--show');
- // });
+  if (hide_rate_btn) {
+    hide_rate_btn.addEventListener('click', function(){
+      hidden_rate.classList.remove('hidden-rates--show');
+    });
+  }
 
-  country_list.addEventListener('click', function(event){
+  if (country_list) {
+    country_list.addEventListener('click', function(event){
     for (var i = 0; i < letter_list.length; i++) {
       letter_list[i].classList.remove('country-list__letter--active');
-      event.target.classList.add('country-list__letter--active');
+      if (event.target.classList.contains('country-list__letter')) {
+        event.target.classList.add('country-list__letter--active');
+      }
     }
   });
+  }
 
-  //letter_list.addEventListener('click', function(){
-  //  console.log("1");
-  //  for (var i = 0; i <= letter_list.length; i++) {
-  //    letter_list[i].classList.remove('country-list__letter--active');
-  //  }
-  //})
+  if (open_filter) {
+    open_filter.addEventListener('click', function(){
+      filter.classList.remove('filter--hidden');
+    });
+  }
 
+  if (close_filter) {
+    close_filter.addEventListener('click', function(){
+      filter.classList.add('filter--hidden');
+    });
+  }
+
+  if (fellow_filter) {
+    fellow_filter.addEventListener('click', function(event){
+      if (event.target.classList.contains('fellow-filter__open')) {
+        event.target.classList.toggle('fellow-filter__open--opened');
+      }
+    });
+  }
 
 });
